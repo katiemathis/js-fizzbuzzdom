228cd19f5f535c3f544d7f7717b32073eeac3f43
katiepalumbo <palumbo.katie@gmail.com>
2022-01-21 13:59:11 +0100

    aligning code

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let containerElement = document.getElementById('container');
 for (let i = 1; i < 101; i++) {
     console.log('Ciclo; ' + i);
     if (i % 3 === 0 && i % 5 === 0){
-    containerElement.innerHTML += "<div class='fizzbuzz'>fizzbuzz</div>";
+        containerElement.innerHTML += "<div class='fizzbuzz'>fizzbuzz</div>";
     } else if (i % 5 === 0){
         containerElement.innerHTML += "<div class='buzz'>buzz</div>";
     } else if (i % 3 === 0) {
