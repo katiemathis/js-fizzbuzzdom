6ccdfd0cb311c338540c4dbdf826a54e670f4b20
katiepalumbo <palumbo.katie@gmail.com>
2022-01-21 13:55:43 +0100

    esercizio completo

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 let containerElement = document.getElementById('container');
 
-for (let i = 0; i < 100; i++) {
-    console.log('Ciclo; ' + i)
-    containerElement.innerHTML += "<div class='box'>"+ i +"</div>"
+
+
+for (let i = 1; i < 101; i++) {
+    console.log('Ciclo; ' + i);
+    if (i % 3 === 0 && i % 5 === 0){
+    containerElement.innerHTML += "<div class='fizzbuzz'>fizzbuzz</div>";
+    } else if (i % 5 === 0){
+        containerElement.innerHTML += "<div class='buzz'>buzz</div>";
+    } else if (i % 3 === 0) {
+        containerElement.innerHTML += "<div class='fizz'>fizz</div>";
+    } else if (i % 3 !== 0 && i % 5 !== 0){
+        containerElement.innerHTML += "<div class='box'>"+ i +"</div>";
+    }
+
 }
